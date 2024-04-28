a5559118b700a5f891fcf6364e9db9266d8ab94e
Raj <raj@Rajs-MacBook-Pro.local>
2024-04-28 23:17:25 +0530

    fix amazon user agent issue

--- extension.py
+++ extension.py
@@ -1,23 +1,46 @@
 from bs4 import BeautifulSoup
 import requests
-headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36'}
+import pdb
+from my_fake_useragent import UserAgent
+
+ua = UserAgent(family="chrome", os_family="windows")
+
+def get_content(url: str) -> str:
+    """Gets the contents of a remote url.
+    Args:
+        url
+    Returns:
+        The content fetched from remote url.
+    """
+    user_agent = ua.random()
+    while True:
+        content: str = requests.get(url, headers={"User-Agent": user_agent})
+        if "api-services-support@amazon.com" in content.text:
+            user_agent = ua.random()
+            continue
+        break
+
+    return content
+
 def flipkart_search(name):
     try:
         global flipkart
+        # pdb.set_trace()
         name1 = name.replace(" ","+")
         flipkart=f'https://www.flipkart.com/search?q={name1}&otracker=search&otracker1=search&marketplace=FLIPKART&as-show=on&as=off'
         res = requests.get(f'https://www.flipkart.com/search?q={name1}&otracker=search&otracker1=search&marketplace=FLIPKART&as-show=on&as=off')
 
 
         print("\nSearching in flipkart....")
+        # print(res.text)
         soup = BeautifulSoup(res.text,'html.parser')
         
-        if(soup.select('._4rR01T')):
-            flipkart_name = soup.select('._4rR01T')[0].getText().strip().upper()
-            flipkart_image = soup.select('._396cs4')[0]['src']
+        if(soup.select('.KzDlHZ')):
+            flipkart_name = soup.select('.KzDlHZ')[0].getText().strip().upper()
+            flipkart_image = soup.select('.DByuf4')[0]['src']
             if name.upper() in flipkart_name:
-                flipkart_price = soup.select('._30jeq3')[0].getText().strip()
-                flipkart_name = soup.select('._4rR01T')[0].getText().strip()
+                flipkart_price = soup.select('.Nx9bqj')[0].getText().strip()
+                flipkart_name = soup.select('.KzDlHZ')[0].getText().strip()
                 print("Flipkart:")
                 print(flipkart_name)
                 print(flipkart_price)
@@ -38,7 +61,8 @@ def flipkart_search(name):
             flipkart_price='0'
             
         return (flipkart_price, flipkart_image) 
-    except:
+    except E:
+        print(E)
         print("Flipkart: No product found!")  
         print("---------------------------------")
         flipkart_price= '0'
@@ -47,11 +71,14 @@ def flipkart_search(name):
 def amazon_search(name):
     try:
         global amazon
+        # pdb.set_trace()
         name1 = name.replace(" ","-")
         name2 = name.replace(" ","+")
-        amazon=f'https://www.amazon.in/{name1}/s?k={name2}'
-        res = requests.get(f'https://www.amazon.in/{name1}/s?k={name2}',headers=headers)
+        amazon=f'https://www.amazon.in/s?k={name2}&crid=3RLCCEAOBMMC7&sprefix={name2}%2Caps%2C286&ref=nb_sb_noss_1'
+        # amazon=f'https://www.amazon.in/{name1}/s?k={name2}'
+        res = get_content(amazon)
         print("\nSearching in amazon...")
+        print(res.text)
         soup = BeautifulSoup(res.text,'html.parser')
         amazon_page = soup.select('.a-color-base.a-text-normal')
         amazon_page_length = int(len(amazon_page))
@@ -77,7 +104,8 @@ def amazon_search(name):
                     break
                     
         return (amazon_price, amazon_image)
-    except:
+    except Exception as error:
+        print(error)
         print("Amazon: No product found!")
         print("---------------------------------")
         amazon_price = '0'
